c3f9d1cc6db45759ccd6588c9d70166abccfad3c
Rahul Devdhar <rahuldevdhar53139@gmail.com>
2022-04-08 08:52:10 +0800

    first commit

--- src/components/News.js
+++ src/components/News.js
@@ -1,4 +1,4 @@
-import React, {useEffect,useLayoutEffect,useState} from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner'
 import PropTypes from 'prop-types'
@@ -9,14 +9,14 @@ const News = (props) => {
   const [loading, setLoading] = useState(true)
   const [page, setPage] = useState(1)
   const [totalResults, setTotalResults] = useState(0)
- 
-  
+
+
   const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
-  
-   
-  const updateNews = async ()=> {
+
+
+  const updateNews = async () => {
     props.setProgress(10);
     const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apikey}&page=${page}&pageSize=${props.pageSize}`;
     setLoading(true)
@@ -27,64 +27,52 @@ const News = (props) => {
     setArticles(parsedData.articles)
     setTotalResults(parsedData.totalResults)
     setLoading(false)
-   
-    
+
+
     props.setProgress(100);
   }
   useEffect(() => {
     document.title = `${capitalizeFirstLetter(props.category)} - Latest News`;
     updateNews();
   }, [])
- 
+
   const fetchMoreData = async () => {
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apikey}&page=${page+1}&pageSize=${props.pageSize}`;
-    setPage(page+1)
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apikey}&page=${page + 1}&pageSize=${props.pageSize}`;
+    setPage(page + 1)
     let data = await fetch(url);
     let parsedData = await data.json()
     setArticles(articles.concat(parsedData.articles))
     setTotalResults(parsedData.totalResults)
-   
+
   };
-  
-    return (
-      <>
-        <h1 className="text-center" style={{ margin: '40px 0px', marginTop: '90px' }}>Latest News - Top {capitalizeFirstLetter(props.category)} Headlines </h1>
-        {loading && <Spinner />}
-        <InfiniteScroll
-          dataLength={articles?.length}
-          next={fetchMoreData}
-          hasMore={articles.length !== totalResults}
-          loader={<Spinner />}
-        >
-          <div className="container">
-            <div className="row">
-            {articles && articles.length > 0  ? articles.map((element) => { return (
-               <div className="col-md-4" key={element.id}>
-      <NewsItem title={element?.title}
-       description={element?.description}
-        imageUrl={element?.urlToImage} newsUrl={element?.url}
-        author={element?.author}
-         date={element?.publishedAt}
-        source={element?.source.name} />
-    </div> );
-        }) : null }
 
+  return (
+    <>
+      <h1 className="text-center" style={{ margin: '40px 0px', marginTop: '90px' }}>Latest News - Top {capitalizeFirstLetter(props.category)} Headlines </h1>
+      {loading && <Spinner />}
+      <InfiniteScroll
+        dataLength={articles?.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner />}
+      >
+        <div className="container">
 
-
-
-
-
-
-            </div>
+          <div className="row">
+            {articles.map((element) => {
+              return <div className="col-md-4" key={element.url}>
+                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+              </div>
+            })}
           </div>
-        </InfiniteScroll>
+        </div>
+      </InfiniteScroll>
+    </>
+  )
 
-      </>
-
-
-    )
-  
 }
+
+
 News.defaultProps = {
   country: 'in',
   pageSize: 8,
@@ -95,6 +83,6 @@ News.propTypes = {
   country: PropTypes.string,
   pageSize: PropTypes.number,
   category: PropTypes.string,
-
 }
+
 export default News
